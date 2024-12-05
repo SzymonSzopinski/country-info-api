021813a65e792c93991b973d9fd45319ae3bc998
Szymon Szopinski <szymon.szopinski.kontakt@gmail.com>
2024-12-06 00:23:22 +0100

    Initialized Spring Boot Project

---
+++ src/main/java/com/example/country_info_api/CountryInfoApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.country_info_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountryInfoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CountryInfoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/country_info_api/CountryInfoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.country_info_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CountryInfoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
